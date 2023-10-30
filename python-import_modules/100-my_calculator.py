e456612c44e79c91ae075b0f8d36e89fa96b622e
Lisky-pixel <o.ndukaaku@alustudent.com>
2023-10-29 21:46:31 -0700

    Kevine

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-
-def calc():
+if __name__ == "__main__":
+    """
+         imports all functions from a file and handles basic operations.
+    """
     from calculator_1 import add, sub, mul, div
-
-    from sys import argv
-    if len(argv) != 4:
+    import sys
+    length = len(sys.argv) - 1
+    if length != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(argv[1])
-    b = int(argv[3])
-    if argv[2] == "+":
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif argv[2] == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif argv[2] == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif argv[2] == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-if __name__ == "__main__":
-    calc()
+        operators = {"+": add, "-": sub, "*": mul, "/": div}
+        if sys.argv[2] not in operators.keys():
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        x = int(sys.argv[1])
+        y = int(sys.argv[3])
+        z = sys.argv[2]
+        print("{} {} {} = {}".format(x, z, y, operators[z](x, y)))
