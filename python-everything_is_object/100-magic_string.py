46df6085ffda3873c83f9fcbc15cabb874fc0048
simplykevine <umutonikevine2004@gmail.com>
2023-11-21 15:02:35 -0800

    up to date

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.count = getattr(magic_string, 'count', 0) + 1
-    return "BestSchool, " * (magic_string.count - 1) + "BestSchool"
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
