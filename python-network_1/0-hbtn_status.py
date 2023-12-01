ff0b091c84affa69eeaf045676f711f39e1ae66f
simplykevine <umutonikevine2004@gmail.com>
2023-12-01 01:33:56 -0800

    well done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
