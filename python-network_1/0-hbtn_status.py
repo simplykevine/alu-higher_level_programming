1adea4f70c4c0f3cfaecbb7eef442006d208fc96
simplykevine <umutonikevine2004@gmail.com>
2023-12-01 02:29:55 -0800

    up to date

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     import urllib.request
 
     with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-        content = res.read()
+        content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
