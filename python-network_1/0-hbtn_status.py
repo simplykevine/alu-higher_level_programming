009d834382a5d75470e05567fcedd99abdfaa3dc
simplykevine <umutonikevine2004@gmail.com>
2023-12-01 02:45:41 -0800

    up to date

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
-"""
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+
+
+import urllib.request
 
 
 if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
