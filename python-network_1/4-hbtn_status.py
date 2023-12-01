788a05c2361251bfe224a240152e01dc1896e640
simplykevine <umutonikevine2004@gmail.com>
2023-12-01 01:26:13 -0800

    well done

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Script that fetches https://alu-intranet.hbtn.io/status."""
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    req = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
