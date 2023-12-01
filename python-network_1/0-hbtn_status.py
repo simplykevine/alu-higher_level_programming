4cac3fb875eceefe22b8bc7b18092b99b031e57f
simplykevine <umutonikevine2004@gmail.com>
2023-12-01 00:36:59 -0800

    up to date

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Import urllib library."""
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+
 
 import urllib.request
 
 
-"""fetch the url then."""
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
