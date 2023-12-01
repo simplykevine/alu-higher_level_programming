d3681363bd05fec0e579ba9e0ed107a2bb8eb5c7
simplykevine <umutonikevine2004@gmail.com>
2023-12-01 01:01:34 -0800

    kevine

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-'''a Python script that fetches a url'''
-
-
-import urllib.request
+"""A script that
+- fetches https://alu-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
+    import urllib.request
+
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
+"""Takes a URL, sends a request and displays the value of X-Request-Id"""
 
-Usage: ./1-hbtn_header.py <URL>
-"""
-import sys
 import urllib.request
-
+import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader("X-Request-Id"))
