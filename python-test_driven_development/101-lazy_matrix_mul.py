8c2015fe06bfd93040c988c7a98ddce22e60ff71
simplykevine <umutonikevine2004@gmail.com>
2023-12-04 06:37:59 -0800

    kevine

---
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a matrix multiplication function using NumPy."""
+'''
+File_name: 
+Created: 5-DEC-2023
+Author: UMUTONI Kevine (simplykevine)
+Size: Large
+Project: python-test_driven_development
+Status: Not yet submitted.
+'''
+
+import numpy as np\
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Return the multiplication of two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
