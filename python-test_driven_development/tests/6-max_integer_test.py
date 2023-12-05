b3164f7aa61f8d717b81dc66d19be337a31d7eb8
simplykevine <umutonikevine2004@gmail.com>
2023-12-04 23:33:06 -0800

    kevine

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+""""Function documentation"""
 
 
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+import unittest
+
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """"Function documentation"""
+
+    def test_simple_case(self):
+        """"Function documentation"""
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+
+    def test_negative_case(self):
+        """"Function documentation"""
+        self.assertEqual(max_integer([-1, -2, -3]), -1)
+
+    def test_empty_case(self):
+        """"Function documentation"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_max_middle_case(self):
+        """"Function documentation"""
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+
+    def test_one_element_case(self):
+        """"Function documentation"""
+        self.assertEqual(max_integer([5]), 5)
