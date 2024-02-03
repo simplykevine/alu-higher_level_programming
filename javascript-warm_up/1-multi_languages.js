eaff8f9cc302361a5a054755537dffde5c0d6854
simplykevine <umutonikevine2004@gmail.com>
2024-02-03 08:49:42 -0800

    java

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const cString = "C is fun";
-const pyString = "Python is cool";
-const jsString = "JavaScript is amazing";
+const cString = 'C is fun';
+const pyString = 'Python is cool';
+const jsString = 'JavaScript is amazing';
 
 console.log(cString);
 console.log(pyString);
